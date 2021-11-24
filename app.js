6f933c8c9cde4b7ba02d8354b1c5b0a72de92f52
Dahye <dhk1417@gmail.com>
2021-11-24 21:08:45 +0900

    수정

--- app.js
+++ app.js
@@ -65,6 +65,11 @@ app.post('/failure', (req, res) => {
  res.redirect("/")
 });
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+ 
+app.listen(port, function() {
+  console.log("Server started succesfully");
 });
