b6ca038dd10189a297395efeb6046d468619a87a
Dahye <dhk1417@gmail.com>
2021-11-24 21:03:21 +0900

    Change

--- app.js
+++ app.js
@@ -65,7 +65,6 @@ app.post('/failure', (req, res) => {
  res.redirect("/")
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("server is running on port 3000")
-
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
